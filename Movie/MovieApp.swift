492e2a2d73fa9bb5744a6d746bcb48a214214d7e
Yuru Zhou <yuz101@ucsd.edu>
2022-01-18 11:11:24 -0800

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Yuru Zhou on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
